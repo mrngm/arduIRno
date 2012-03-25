9df8b5f948808fc0ea3fcd8d3910025254c57e6c
mrngm <mrngm@moeilijklastig.nl>
2012-03-25 19:07:34 +0200

    Rewrote test to tokenize and extract with variable vendor length

--- test/blaat.cpp
+++ test/blaat.cpp
@@ -1,17 +1,40 @@
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <string>
 
 int
 main (int argc, char** argv) {
 
-	char params[] = "NEC_0x88877_32";
-	char* vendor = new char[4];
+	char params[] = "Sony_0x88877_32";
+	char* vendor;
 	unsigned long int code = 0;
 	unsigned int bits = 0;
 
-	std::cout << "Scanning params: '" << params << "'" << std::endl;
-	std::cout << "sscanf: " << sscanf(params, "%3s_0x%5lx_%2u", vendor, &code, &bits) << std::endl; 
+	char * pch;
+	char * tokens = new char[strlen(params)];
+	strcpy(tokens, params);
+	int num = 0;
+	pch = strtok(tokens, "_");
+	while (pch != NULL) {
+		num++;
+		switch (num) {
+			case 1:
+				vendor = new char[strlen(pch)];
+				strcpy(vendor, pch);
+				break;
+			case 2:
+				sscanf(pch, "0x%5lx", &code);
+				break;
+			case 3:
+				sscanf(pch, "%2u", &bits);
+				break;
+			default:
+				break;
+		}
+		pch = strtok (NULL, "_");
+	}
+	std::cout << "Extracted data from params: '" << params << "'" << std::endl;
 	std::cout << "Vendor: " << vendor << std::endl;
 	std::cout << "Code  : " << code   << std::endl;
 	std::cout << "Bits  : " << bits   << std::endl;
